b70f9fa62d60b3b5e14cf814dc0ee6c94ede476b
Bonnie Schulkin <bonnie@hackbrightacademy.com>
2016-11-07 12:54:46 -0800

    updated model and seed file to create and store markov chains

--- model.py
+++ model.py
@@ -1,6 +1,9 @@
 """data model for music markov chains"""
 
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm.exc import NoResultFound
+from fractions import Fraction
+import music21 as m21
 
 # This is the connection to the PostgreSQL database; we're getting this through
 # the Flask-SQLAlchemy helper library. On this, we can find the `session`
@@ -12,105 +15,265 @@ db = SQLAlchemy()
 ##############################################################################
 # Model definitions
 
-class Note(db.Model):
-    """User of ratings website."""
+class Duration(db.Model):
+    """note duration"""
 
-    __tablename__ = "notes"
+    __tablename__ = 'durations'
+
+    duration_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+
+    # this is in number of quarter notes (e.g. an eighth note would be 0.5)
+    quarter_notes = db.Column(db.Float, nullable=False)
+
+    @classmethod
+    def add(cls, quarter_notes):
+        """given length in quarter notes, instantiate a duration and add to db 
+
+        Will just return existing duration object if it's already there
+
+        returns duration obj"""
+
+        # translate fraction to float
+        if isinstance(quarter_notes, Fraction):
+            quarter_notes = float(quarter_notes)
+
+        try:
+            duration = cls.query.filter_by(quarter_notes=quarter_notes).one()
+        except NoResultFound:
+            duration = cls(quarter_notes=quarter_notes)
+
+        db.session.add(duration)
+        return duration
+
+    def __repr__(self):
+        """Provide helpful representation when printed."""
+
+        return "<Duration duration_id=%s quarter_notes=%s>" % \
+                (self.duration_id, self.quarter_notes)
+
+
+class Note(db.Model):
+    """Note, including pitch and duration"""
+
+    __tablename__ = 'notes'
 
     note_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
 
+    # pitch; will be null for a rest
+    note_name = db.Column(db.String(3), nullable=True)
+    octave = db.Column(db.Integer, nullable=True)
+
+    duration_id = db.Column(db.Integer, db.ForeignKey('durations.duration_id'))
+    tempo_id = db.Column(db.Integer, db.ForeignKey('tempi.tempo_id'))
+
+    ## relationships ##
+    duration = db.relationship('Duration')
+    tempo = db.relationship('Tempo')
+
+    @classmethod
+    def add(cls, m21_note, tune, index):
+        """create note given a music21 note object, a tune obj, and an index 
+
+        Also, add note to tune. 
+
+        If note already exists, don't create a new one, just add it to the tune
+
+        Returns note object
+        """
+
+        # create a duration if necessary
+        duration = Duration.add(m21_note.duration.quarterLength)
+
+        try:
+            pitch = m21_note.pitch
+            note_name = pitch.name
+            octave = pitch.octave
+        except AttributeError:
+            # rests have no name or octave, so it must be a rest
+            note_name = None
+            octave = None
+
+        # does this note already exist? 
+        try: 
+            new_note = Note.query.filter_by(note_name=note_name, 
+                                            octave=octave, 
+                                            duration_id=duration.duration_id).one()
+
+        except NoResultFound: 
+            new_note = cls(note_name=note_name, octave=octave)
+            new_note.duration = duration
+            db.session.add(new_note)
+
+        # add note to tune
+        db.session.flush()
+        tunenote = TuneNote(tune_id=tune.tune_id, note_id=new_note.note_id, index=index)
+
+        return new_note
+
     def __repr__(self):
         """Provide helpful representation when printed."""
 
-        return "<Note note_id=%s =%s>" % (self.note_id, self.)
+        return "<Note note_id=%s note_name=%s octave=%s quarter notes=%s>" % \
+                (self.note_id, self.note_name, self.octave, self.duration.quarter_notes)
+
+
+class Tempo(db.Model):
+    """musical tempo"""
+
+    __tablename__ = 'tempi'
+
+    tempo_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    text = db.Column(db.String(32), nullable=True)
+    seconds_per_quarter = db.Column(db.Float, nullable=False)
+
+    @classmethod
+    def add(cls, m21_score):
+        """given a music21 score obj, instantiate a tempo and add to db 
+
+        Will just return existing tempo object if it's already there
+
+        returns tempo obj"""
+
+        # the [0] means we're just taking the first tempo of the score
+        metronome_mark =  m21_score.metronomeMarkBoundaries()[0][2]
+        spq = metronome_mark.secondsPerQuarter()
+        text = metronome_mark.text
+
+        try:
+            tempo = Tempo.query.filter_by(text=text, seconds_per_quarter=spq).one()
+        except NoResultFound:
+            tempo = Tempo(text=text, seconds_per_quarter=spq)
+
+        db.session.add(tempo)
+        return tempo
+
+
+    def __repr__(self):
+        """Provide helpful representation when printed."""
+
+        return "<Tempo id=%s text=%s seconds/quarter=%s>" % \
+                (self.tempo_id, self.text, self.seconds_per_quarter)
+
+
+class TuneNote(db.Model):
+    """associating tunes with notes"""
+
+    __tablename__ = 'tunenotes'
+    
+    tunenote_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    tune_id = db.Column(db.Integer, db.ForeignKey('tunes.tune_id'))
+    note_id = db.Column(db.Integer, db.ForeignKey('notes.note_id'))
+
+    # what number is this in the tune, starting with 0
+    index = db.Column(db.Integer)
+
+    def __repr__(self):
+        """Provide helpful representation when printed."""
+
+        return "<TuneNote tunenote_id=%s tune_id=%s note_id=%s>" % \
+                (self.tunenote_id, self.tune_id, self.note_id)
+
+
+class Tune(db.Model):
+    """A tune containing notes"""
+
+    __tablename__ = 'tunes'
+
+    tune_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    tune_name = db.Column(db.String(256))
+    tempo_id = db.Column(db.Integer, db.ForeignKey('tempi.tempo_id'))
+
+    ### relationships ###
+    tempo = db.relationship('Tempo')
+    notes = db.relationship('Note', 
+                            secondary='tunenotes', 
+                            order_by='TuneNote.index', 
+                            backref='tunes')
+
+    @classmethod
+    def add(cls, name, tempo):
+        """given a name and a tempo, create a tune obj and add to db 
+
+        returns tune obj"""
+
+        tune = cls(tune_name=name, tempo_id=tempo.tempo_id)
+        return tune
+
+    def __repr__(self):
+        """Provide helpful representation when printed."""
+
+        return "<Tune tune_id=%s name=%s tempo_id=%s>" % \
+                (self.tune_id, self.name, self.tempo_id)
 
     
-    def predict_rating(self, movie):
-        """Predict user's rating of a movie."""
+class Chain(db.Model):
+    """table to track Markov sequences"""
 
-        start =  datetime.now()
+    __tablename__ = 'chains'
 
-        other_ratings = movie.ratings
+    chain_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    note1_id = db.Column(db.Integer, db.ForeignKey('notes.note_id'))
+    note2_id = db.Column(db.Integer, db.ForeignKey('notes.note_id'))
 
-        similarities = [
-            (self.similarity(r.user), r)
-            for r in other_ratings
-        ]
+    @classmethod
+    def add(cls, note1, note2):
+        """given two note objects, instantiate a chain (or return existing chain)
 
-        similarities.sort(reverse=True)
+        returns a chain object.
+        """
+        
+        try: 
+            chain = Chain.query.filter_by(note1_id=note1.note_id, 
+                                          note2_id=note2.note_id).one()
+        except NoResultFound:
+            chain = cls(note1_id=note1.note_id, note2_id = note2.note_id)
+            db.session.add(chain)
 
-        similarities = [(sim, r) for sim, r in similarities if sim > 0]
-
-        if not similarities:
-            return None
-
-        numerator = sum([r.score * sim for sim, r in similarities])
-        denominator = sum([sim for sim, r in similarities])
-
-        print "************* prediction for [", movie.title, "] took", datetime.now() - start
-
-        return numerator/denominator
-
-    def similarity(self, other):
-        """Return Pearson rating for user compared to other user."""
-
-        u_ratings = {}
-        paired_ratings = []
-
-        for r in self.ratings:
-            u_ratings[r.movie_id] = r
-
-        for r in other.ratings:
-            u_r = u_ratings.get(r.movie_id)
-            if u_r:
-                paired_ratings.append( (u_r.score, r.score) )
-
-        if paired_ratings:
-            return correlation.pearson(paired_ratings)
-
-        else:
-            return 0.0
-
-
-class Movie(db.Model):
-    """Movie on ratings website."""
-
-    __tablename__ = "movies"
-
-    movie_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    title = db.Column(db.String(100))
-    released_at = db.Column(db.DateTime)
-    imdb_url = db.Column(db.String(200))
+        return chain
 
     def __repr__(self):
         """Provide helpful representation when printed."""
 
-        return "<Movie movie_id=%s title=%s>" % (self.movie_id, self.title)
+        return "<Chain chain_id=%s notes=%s, %s>" % \
+                (self.chain_id, self.note1_id, self.note2_id)
+
+class NextNote(db.Model):
+    """track the next note possibilities for each Markov sequence"""
+
+    __tablename__ = 'nextnote'
+
+    nextnote_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    chain_id = db.Column(db.Integer, db.ForeignKey('chains.chain_id'))
+    note_id = db.Column(db.Integer, db.ForeignKey('notes.note_id'))
+    weight = db.Column(db.Integer, default=1)
+
+    @classmethod
+    def add(cls, chain, note):
+        """given a chain obj and a note obj, instantiate a nextnote object
+
+        OR: add to the weight of the nextnote object if it already exists
+        """
+
+        try:
+            nextnote = cls.query.filter_by(chain_id=chain.chain_id, note_id=note.note_id).one()
+
+            # if we got this far, it exists; add to weight
+            nextnote.weight = nextnote.weight + 1
+
+        except NoResultFound:
+            # doesn't exist; make a new one
+            nextnote = cls(chain_id=chain.chain_id, note_id=note.note_id)
 
 
-class Rating(db.Model):
-    """Rating of a movie by a user."""
+        db.session.add(nextnote)
+        return nextnote
 
-    __tablename__ = "ratings"
-
-    rating_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    movie_id = db.Column(db.Integer, db.ForeignKey('movies.movie_id'))
-    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
-    score = db.Column(db.Integer)
-
-    # Define relationship to user
-    user = db.relationship("User",
-                           backref=db.backref("ratings", order_by=rating_id))
-
-    # Define relationship to movie
-    movie = db.relationship("Movie",
-                            backref=db.backref("ratings", order_by=rating_id))
 
     def __repr__(self):
         """Provide helpful representation when printed."""
 
-        return "<Rating rating_id=%s movie_id=%s user_id=%s score=%s>" % (
-            self.rating_id, self.movie_id, self.user_id, self.score)
+        return "<NextNote nextnote_id=%s chain_id=%s, note_id=%s, weight=%s>" % \
+                (self.nextnote_id, self.chain_id, self.note_id, self.weight)
 
 
 ##############################################################################
@@ -132,6 +295,9 @@ if __name__ == "__main__":
 
     from flask import Flask
     app = Flask(__name__)
-
     connect_to_db(app)
+
+    # db.drop_all()
+    db.create_all()
+
     print "Connected to DB."
---
+++ seed.py
@@ -0,0 +1,79 @@
+"""seed database with notes and markov chains"""
+
+from model import db, connect_to_db, Note, NextNote, Tempo, Chain, Tune
+import os
+from music21 import note, corpus, converter
+from music21.metadata import Metadata
+
+BACH_DATADIR = 'data/bach_cello_suites'
+MIDI_EXT = 'mid'
+
+def markovify_score(filepath, score, notecount, markovcount):
+    """add markov chains to db using music21 score object"""
+
+    # get / make the tempo and tune objects
+    tempo = Tempo.add(score)
+    tune = Tune.add(filepath, tempo)
+
+    # for now, only look at first part to the score (parts are like parallel 
+    # voices -- choral voices, for example, or for tracking double stops on the
+    # cello). In the future...?
+    part = score.parts[0]
+    part_notes = part.notesAndRests
+
+    print "\ttotal notes and rests", len(part_notes)
+
+    # get the first two notes in preparation for markovification
+    note_a = Note.add(part_notes[0], tune, 0)
+    note_b = Note.add(part_notes[1], tune, 1)
+
+    # let the markovification begin!
+    for index, part_note in enumerate(part_notes[2:]):
+
+        # make the chain
+        markov = Chain.add(note_a, note_b)
+
+        # add the note to the db and add it to the chain
+        note_c = Note.add(part_note, tune, index + 2)
+        NextNote.add(markov, note_c)
+
+        # move forward one
+        (note_a, note_b) = (note_b, note_c)
+        db.session.flush()
+
+        # so we make new objects to add, rather than changing existing ones
+        del markov, note_c 
+
+    db.session.commit()
+
+    new_notecount = Note.query.count()
+    new_markovcount = Chain.query.count()
+
+    print '\tadded', new_notecount - notecount, 'notes'
+    print '\tadded', new_markovcount - markovcount, 'chains'
+
+    return new_notecount, new_markovcount
+
+
+def load_bachdata():
+    """load data from midi bach files in BACH_DATADIR"""
+
+    notecount = 0
+    markovcount = 0
+
+    for filepath in os.listdir(BACH_DATADIR):
+        if filepath.endswith(MIDI_EXT): 
+            print "processing", filepath
+            score = converter.parse(os.path.join(BACH_DATADIR, filepath))
+            notecount, markovcount = markovify_score(filepath, score, notecount, markovcount)
+
+if __name__ == "__main__":
+
+    from flask import Flask
+    app = Flask(__name__)
+    connect_to_db(app)
+
+    db.drop_all()
+    db.create_all()
+
+    load_bachdata()
